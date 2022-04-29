91619b5ae8152638f308d34d2e53a4883988df70
Your NameDenis <tdv14011984@gmail.com>
2022-04-29 20:24:38 +0300

    add resources

--- src/test/java/denis/timushev/ParseZipTest.java
+++ src/test/java/denis/timushev/ParseZipTest.java
@@ -1,5 +1,25 @@
 package denis.timushev;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ParseZipTest {
 
+    ClassLoader classLoader = getClass().getClassLoader();
+
+    @Test
+    void zipTest() throws Exception {
+        try (InputStream is = classLoader.getResourceAsStream("files/FilesForTest.zip");
+             ZipInputStream zis = new ZipInputStream(is)) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                assertThat(entry.getName()).isEqualTo("NDFL.xlsx");
+            }
+        }
+    }
 }
